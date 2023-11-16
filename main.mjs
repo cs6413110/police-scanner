80405c361f1d651618169422711a2475450f4dc7
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-11-16 08:35:02 -0700

    test streaming data from the police radio

--- main.mjs
+++ main.mjs
@@ -4,37 +4,34 @@
   Audio to Text conversion by assemblyai(NOT 100% FREE)
 */
 
-import {AssemblyAI} from 'assemblyai';
 import fetch from 'node-fetch';
-const client = new AssemblyAI({apiKey: '19b97f1b12024a1fae253a33724f32c7'});
-const realtime = client.realtime.createService({sampleRate: 16_000});
-const policeRadioSources = {}
+import {ChatGPTUnofficialProxyAPI} from 'chatgpt';
+import { nodewhisper } from 'nodejs-whisper'
+
+const chatgpt = new ChatGPTUnofficialProxyAPI({accessToken: ''});  
+
+const policeRadioSources = {}, scanners = [], events = [];
 policeRadioSources['Mesa Police Department Central Patrol District'] = 'https://listen.broadcastify.com/nqhkys674dzfmvx.mp3?nc=66398&xan=xtf9912b41c';
+//policeRadioSources['Mesa Police Department Fiesta Patrol District'] = '';
 
-(async() => {
-  realtime.on('open', ({sessionId}) => console.log('Session created with id: '+sessionId));
-  realtime.on('error', error => console.error(error));
-  realtime.on('close', (code, reason) => console.log('Session closed:', code, reason));
-  realtime.on('transcript', transcript => {
-    console.log('Received:', transcript)
-    if (!transcript.text) return;
-    if (transcript.message_type === 'FinalTranscript') {
-      console.log('Final:', transcript.text);
-    } else {
-      console.log('Partial:', transcript.text);
-    }
-  });
+class PoliceScanner {
+  constructor() {
+    this.data = [];
+    this.res = await fetch(policeRadioSources['Mesa Police Department Central Patrol District']);
+    this.res.body.on('data', buffer => {
+      console.log('Data received: '+buffer);
+      this.data.push(buffer);
+    });
+    setInterval(() => this.whispr(), 15000); // translate data to text via whispr every 15 seconds
+  }
 
-  console.log('Connecting to real-time transcript service');
-  await realtime.connect();
-  const res = await fetch(policeRadioSources['Mesa Police Department Central Patrol District']);
-  res.body.on('data', buffer => {
-    console.log('fetch received data');
-    realtime.sendAudio(buffer);
-  });
-})();
-
-class RadioListener {
+  whispr() {
+    
+  }
 
   
 }
+
+for (const source of policeRadioSources) scanners.push(new PoliceScanner(source));
+
+
