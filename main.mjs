8263ae26aab9fccd04bf9c198cb9f6f289281c55
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-11-16 11:54:33 -0700

    Update main.mjs

--- main.mjs
+++ main.mjs
@@ -43,7 +43,7 @@ class PoliceScanner {
 
   async whispr() {
     for (const filename of this.filesToProcess) {
-      const transcript = await whisper(`${filename}`);
+      const transcript = await whisper(`${filename}`, {modelName: 'tiny.en'});
       textToProcess.push(transcript.speech);
     }
   }  
