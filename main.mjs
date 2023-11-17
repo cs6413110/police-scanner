258a154c828d880165c752a0a30a58af854a10e3
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-11-17 09:10:59 -0700

    new gpt

--- main.mjs
+++ main.mjs
@@ -18,16 +18,23 @@ import fetch from 'node-fetch';
 import ffmpeg from 'fluent-ffmpeg';
 import {fileURLToPath} from 'url';
 import {dirname, resolve} from 'path';
-import {ChatGPTUnofficialProxyAPI} from 'chatgpt';
+import {gpt} from 'gpti';
 import {nodewhisper as whisper} from 'nodejs-whisper';
 
 const __filename = fileURLToPath(import.meta.url), __dirname = dirname(__filename);
-
-const chatgpt = new ChatGPTUnofficialProxyAPI({accessToken: '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'}); 
 const prompt = 'You are a police radio scanner. Your job is to take the provided radio text and use the information to provided data to a safety application to notify home owners of nearby crime. You will provided a response with data structured like so: [{"address":"<Address of the event, defaults to UNKNOWN>","starttime":"<time of the occurance, defaults to RECENTLY">,"type":"<Type of the event(e.g robbery, break-in, assult, threat...), defaults to UNKNOWN>"}]. Here is the police radio stream in a text format for you to process: ';
 (async() => {
-  const res = await chatgpt.sendMessage('hi');
-  console.log(JSON.stringify(res));
+  gpt({
+    prompt: "hello gpt, tell me what your version is?",
+    model: "gpt-4",                         // code or model
+    type: "json"                            // optional: "json" or "markdown"
+  }, (err, data) => {
+    if(err != null){
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+  });
 })();
 
 
@@ -73,8 +80,17 @@ class PoliceScanner {
 
   async chatgpt() {
     console.log('Applying chatgpt to: '+this.transcript);
-    const res = await chatgpt.sendMessage(prompt+this.transcript);
-    console.log(res.text);    
+    gpt({
+      prompt: prompt+this.transcript,
+      model: 'gpt-4',
+      type: 'json'
+    }, (err, data) => {
+      if (err !== null) {
+        console.log(err);
+      } else {
+        console.log(data);
+      }
+    });
   }
 }
 
