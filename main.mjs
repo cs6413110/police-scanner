d89fb3e858902d2aba1a2914560accd4e8c4e1da
cs6413110 <129222222+cs6413110@users.noreply.github.com>
2023-11-17 15:21:28 -0700

    Update main.mjs

--- main.mjs
+++ main.mjs
@@ -75,9 +75,10 @@ class PoliceScanner {
     this.res.body.pipe(this.file); // Link to mp3 stream
     setTimeout(() => this.file.end(), 1000*30); // File size will be ~10 minute longs
     this.file.on('finish', () => {
-      ffmpeg(resolve(__dirname, this.fileSource)).toFormat('wav').outputOptions('-ar 16000').save(resolve(__dirname, this.fileSource).replace('mp3', 'wav'));
-      this.filesToProcess.push(this.fileSource);
-      this.makeFileStream();
+      ffmpeg(resolve(__dirname, this.fileSource)).toFormat('wav').outputOptions('-ar 16000').on('end', () => {
+        this.filesToProcess.push(this.fileSource);
+        this.makeFileStream();
+      }).save(resolve(__dirname, this.fileSource).replace('mp3', 'wav'));
     }); // After stream is 100% done, link a new stream
   }
 
